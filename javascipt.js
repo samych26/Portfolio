65c52694692ac261081bb88e92d773ee356b837d
samy <samy.cherfa@se.univ-bejaia.com>
2024-05-19 01:21:56 +0200

    Initial commit

---
+++ javascipt.js
@@ -0,0 +1,7 @@
+function basculeMenu(){
+    const menu = document.querySelector(".menu-links")
+    const icone = document.querySelector(".icone-resp")
+menu.classList.toggle("open")
+icone.classList.toggle("open")
+    
+}
